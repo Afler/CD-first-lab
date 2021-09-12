b74537581ec70ee9fceb0de51d33445d6a32c8dd
Lev <afler777@gmail.com>
2021-09-12 20:39:15 +0400

    potentially fixed

--- first_lab.py
+++ first_lab.py
@@ -5,23 +5,22 @@ def REF(B):
     A = B.copy()
     m = A.shape[0]  # rows number
     n = A.shape[1]  # columns number
+    number_of_leaders = 0
     for j in range(n):  # by columns
         lead = -1
-        for i in range(j, m):  # by rows
+        for i in range(number_of_leaders, m):  # by rows
             if A[i, j] != 0:
                 lead = A[i, j]
-                if i != j:
-                    A[[i, j]] = A[[j, i]]
+                A[[i, number_of_leaders]] = A[[number_of_leaders, i]]
                 A[i] = A[i] / lead % 2
-                for k in range(i + 1, m):
-                    A[k] = (A[k] - A[j] * A[k, j]) % 2
+                for k in range(number_of_leaders + 1, m):
+                    A[k] = (A[k] - A[number_of_leaders] * A[k, j]) % 2
+                number_of_leaders += 1
     return A
 
 
 if __name__ == '__main__':
-    A = np.mat([[0, 0, 0],
-                [1, 0, 1],
+    A = np.mat([[0, 1, 1],
+                [0, 0, 1],
                 [0, 1, 1]], dtype=int)
-
-    A = REF(A)
-    print(A)
+    print(REF(A))
