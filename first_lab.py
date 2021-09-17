b2f0172616244e9c39c8c1bff3232c82b2502c55
Lev <afler777@gmail.com>
2021-09-17 18:29:54 +0400

    1.4.2 in progress

--- first_lab.py
+++ first_lab.py
@@ -33,8 +33,11 @@ class LinearCode:
             if np.array_equal(np.ravel(self.G[i]), np.zeros(n, dtype=int)):
                 self.G = self.G[0: i]
                 break
+        print("G =\n", self.G)
         self.n = self.G.shape[1]
         self.k = self.G.shape[0]
+        print("n = ", self.n)
+        print("k = ", self.k)
         self.X = RREF(self.G)
         delete_columns = []
         for i in range(self.X.shape[0]):
@@ -42,7 +45,7 @@ class LinearCode:
                 if self.X[i, j] != 0:
                     delete_columns.append(j)
                     break
-        print(delete_columns)
+        print("lead = ", delete_columns)
         self.X = np.delete(self.X, delete_columns, 1)
         I = np.mat(np.eye(self.X.shape[1], dtype=int))
         print("I =\n", I)
@@ -59,6 +62,7 @@ class LinearCode:
                 iter_k += 1
         print("H =\n", self.H)
 
+        # task 1.4.1
         allowed_words = self.G.copy()
         for i in range(self.G.shape[0] - 1):
             for j in range(i + 1, self.G.shape[0]):
@@ -71,6 +75,25 @@ class LinearCode:
                 if isAppend:
                     allowed_words = np.vstack([allowed_words, row])
         print("allowed words =\n", allowed_words)
+
+        # task 1.4.2
+        I = np.mat(np.eye(self.k, dtype=int))
+        k_length_words = I.copy()
+        index_to_add = I.shape[0]
+        for i in range(index_to_add):
+            for j in range(0, I.shape[0]):
+                row = (I[i] + I[j]) % 2
+                isNotAppend = True
+                for k in range(0, k_length_words.shape[0]):
+                    if np.array_equal(k_length_words[k], row):
+                        isNotAppend = False
+                        break
+                if isNotAppend:
+                    k_length_words = np.vstack([k_length_words, row])
+                    index_to_add += 1
+
+        print("k_length_words =\n", k_length_words)
+        print(k_length_words @ self.G)
 
 
 def REF(B):
@@ -109,11 +132,6 @@ if __name__ == '__main__':
                 [0, 0, 1, 1, 1],
                 [1, 1, 1, 0, 1],
                 [1, 1, 0, 0, 1]], dtype=int)
-    # print(REF(A))
-    # print(RREF(A))
+    print("REF(A) =\n", REF(A))
+    print("RREF(A) = \n", RREF(A))
     linearcode = LinearCode(A)
-    # print(linearcode.A)
-    print("G =\n", linearcode.G)
-    # print(linearcode.n)
-    # print(linearcode.k)
-    # print(linearcode.X)
