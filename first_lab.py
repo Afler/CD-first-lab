c9d04fb272dd9a92b7d574b6d30a242a1cbcaa4b
Lev <afler777@gmail.com>
2021-09-18 12:39:21 +0400

    some fixes

--- first_lab.py
+++ first_lab.py
@@ -1,55 +1,66 @@
 import numpy as np
 
 
+def printMatrix(arr, name):
+    print(name, " = ")
+    print(arr)
+
+
 class LinearCode:
-    A = np.mat([[]], dtype=int)
     G = np.mat([[]], dtype=int)
+    n = 0
+    k = 0
     X = np.mat([[]], dtype=int)
     H = np.mat([[]], dtype=int)
     allowed_words = np.mat([[]], dtype=int)
-    n = 0
-    k = 0
 
-    def __init__(self, A):
-        m = A.shape[0]
-        n = A.shape[1]
-        B = A.copy()
+    def __init__(self, S):
+
+        # task 1.3.1
+        m = S.shape[0]
         for i in range(m - 1):
             for j in range(i + 1, m):
-                row = (A[i] + A[j]) % 2
+                row = (S[i] + S[j]) % 2
                 isAppend = True
-                for k in range(0, B.shape[0]):
-                    if np.array_equal(B[k], row):
+                for k in range(0, S.shape[0]):
+                    if np.array_equal(S[k], row):
                         isAppend = False
                         break
                 if isAppend:
-                    B = np.vstack([B, row])
-        B = np.vstack([B, np.zeros(n, dtype=int)])
-        self.A = B
-        self.G = RREF(self.A)
+                    S = np.vstack([S, row])
+        S = np.vstack([S, np.zeros(S.shape[1], dtype=int)])
+        self.G = RREF(S)
         m = self.G.shape[0]
         n = self.G.shape[1]
         for i in range(m):
             if np.array_equal(np.ravel(self.G[i]), np.zeros(n, dtype=int)):
                 self.G = self.G[0: i]
                 break
-        print("G =\n", self.G)
+        printMatrix(self.G, "G")
+
+        # task 1.3.2
         self.n = self.G.shape[1]
         self.k = self.G.shape[0]
-        print("n = ", self.n)
-        print("k = ", self.k)
+        print("n =", self.n)
+        print("k =", self.k)
+
+        # task 1.3.3
+        # step 1
         self.X = RREF(self.G)
+        # step 2
         delete_columns = []
         for i in range(self.X.shape[0]):
             for j in range(self.X.shape[1]):
                 if self.X[i, j] != 0:
                     delete_columns.append(j)
                     break
-        print("lead = ", delete_columns)
+        print("lead =", delete_columns)
+        # step 3
         self.X = np.delete(self.X, delete_columns, 1)
+        # step 4
         I = np.mat(np.eye(self.X.shape[1], dtype=int))
-        print("I =\n", I)
-        print("X =\n", self.X)
+        printMatrix(I, "I")
+        printMatrix(self.X, "X")
         self.H = np.zeros([self.X.shape[0] + I.shape[0], self.X.shape[1]], dtype=int)
         iter_k = 0
         iter_j = 0
@@ -60,7 +71,7 @@ class LinearCode:
             else:
                 self.H[[i]] = I[iter_k]
                 iter_k += 1
-        print("H =\n", self.H)
+        printMatrix(self.H, "H")
 
         # task 1.4.1
         allowed_words = self.G.copy()
@@ -74,7 +85,7 @@ class LinearCode:
                         break
                 if isAppend:
                     allowed_words = np.vstack([allowed_words, row])
-        print("allowed words =\n", allowed_words)
+        printMatrix(allowed_words, "allowed words")
 
         # task 1.4.2
         I = np.mat(np.eye(self.k, dtype=int))
@@ -83,17 +94,16 @@ class LinearCode:
         for i in range(index_to_add):
             for j in range(0, I.shape[0]):
                 row = (I[i] + I[j]) % 2
-                isNotAppend = True
+                isAppend = True
                 for k in range(0, k_length_words.shape[0]):
                     if np.array_equal(k_length_words[k], row):
-                        isNotAppend = False
+                        isAppend = False
                         break
-                if isNotAppend:
+                if isAppend:
                     k_length_words = np.vstack([k_length_words, row])
                     index_to_add += 1
-
-        print("k_length_words =\n", k_length_words)
-        print(k_length_words @ self.G)
+        printMatrix(k_length_words, "k_length_words")
+        printMatrix(k_length_words @ self.G % 2, "k@G")
 
 
 def REF(B):
@@ -123,7 +133,7 @@ def RREF(B):
             if C[i, j] != 0:
                 for k in range(i - 1, -1, -1):
                     C[k, j] = 0
-
+                break
     return C
 
 
@@ -132,6 +142,9 @@ if __name__ == '__main__':
                 [0, 0, 1, 1, 1],
                 [1, 1, 1, 0, 1],
                 [1, 1, 0, 0, 1]], dtype=int)
-    print("REF(A) =\n", REF(A))
-    print("RREF(A) = \n", RREF(A))
-    linearcode = LinearCode(A)
+    S = np.mat([[1, 0, 1, 1, 0, 0, 0, 1, 0, 0],
+                [0, 0, 0, 1, 1, 1, 0, 1, 0, 1],
+                [0, 0, 0, 0, 1, 0, 0, 1, 0, 0],
+                [1, 0, 1, 0, 1, 1, 1, 0, 0, 0],
+                [0, 0, 0, 0, 1, 0, 0, 1, 1, 1]])
+    linearcode = LinearCode(S)
