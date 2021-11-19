be7c076ff8c07d0324a86a11629b408154761e7d
Vadim <9536611@gmail.com>
2021-11-19 22:34:35 +0400

    3.1 half

--- third_lab.py
+++ third_lab.py
@@ -20,10 +20,32 @@ class HammingCode:
         self.k = 2 ** r - r - 1
         self.H = np.mat(np.zeros([self.n, r]), dtype=int)
         self.H[self.n - r:self.n + 1][:] = np.mat(np.eye(r, r))
+        addIndex = 0
+        # init H
+        for i in range(0, self.H.shape[0]):
+            for j in range(0, self.H.shape[0]):
+                row = (self.H[[i]] + self.H[[j]]) % 2
+                isAppend = True
+                for k in range(0, self.H.shape[0]):
+                    if np.array_equal(self.H[[k]], row) or np.array_equal(row, np.zeros([1, r])):
+                        isAppend = False
+                        break
+                if isAppend:
+                    self.H[[addIndex]] = row
+                    addIndex = addIndex + 1
+        # init G
+        self.G = np.mat(np.zeros([self.k, self.n]), dtype=int)
+        t = self.G[:][0 : self.n - r]
+        self.G[:, 0 : self.n - r] = np.mat(np.eye(self.k, self.k))
+        self.G[:, self.n - r : self.n] = self.H[0 : self.n - r, :]
 
 
 if __name__ == '__main__':
-    hammingThreeOne = HammingCode(2)
+    hammingThreeOne = HammingCode(3)
+    print(hammingThreeOne.H)
+    print(hammingThreeOne.G)
+    #print(hammingThreeOne.G)
+    #print(hammingThreeOne)
     #hammingSevenFour = HammingCode(3)
     #hammingFifteenEleven = HammingCode(4)
     print("End")
