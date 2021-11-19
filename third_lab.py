bac892f51eee746013780a93f2c8bb63afffc575
Lev <afler777@gmail.com>
2021-11-19 23:20:59 +0400

    task 3.2 done

--- third_lab.py
+++ third_lab.py
@@ -21,6 +21,7 @@ class HammingCode:
         self.H = np.mat(np.zeros([self.n, r]), dtype=int)
         self.H[self.n - r:self.n + 1][:] = np.mat(np.eye(r, r))
         addIndex = 0
+        # task 3.1
         # init H
         for i in range(0, self.H.shape[0]):
             for j in range(0, self.H.shape[0]):
@@ -35,17 +36,34 @@ class HammingCode:
                     addIndex = addIndex + 1
         # init G
         self.G = np.mat(np.zeros([self.k, self.n]), dtype=int)
-        t = self.G[:][0 : self.n - r]
-        self.G[:, 0 : self.n - r] = np.mat(np.eye(self.k, self.k))
-        self.G[:, self.n - r : self.n] = self.H[0 : self.n - r, :]
+        t = self.G[:][0: self.n - r]
+        self.G[:, 0: self.n - r] = np.mat(np.eye(self.k, self.k))
+        self.G[:, self.n - r: self.n] = self.H[0: self.n - r, :]
+        # syndromes
+        syndromes = (np.mat(np.eye(self.n, dtype=int)) @ self.H) % 2
+        printMatrix(syndromes, "syndromes")
+        printMatrix(self.H, "H")
+        printMatrix(self.G, "G")
+        # task 3.2
+        word = self.G[[0]]
+        # Однократная ошибка
+        word[0, 0] = (word[0, 0] + 1) % 2
+        wordSyndrome = (word @ self.H) % 2
+        printMatrix(wordSyndrome, "wordSyndrome1")
+        # Двукратная ошибка
+        word[0, 1] = (word[0, 1] + 1) % 2
+        wordSyndrome = (word @ self.H) % 2
+        printMatrix(wordSyndrome, "wordSyndrome2")
+        # Трехкратная ошибка
+        word[0, 2] = (word[0, 2] + 1) % 2
+        wordSyndrome = (word @ self.H) % 2
+        printMatrix(wordSyndrome, "wordSyndrome3")
+        # Вывод: Код Хеминга действительно исправляет однократные,
+        # обнаруживает двукратные и возникает неопределеность для трехкратных ошибок
 
 
 if __name__ == '__main__':
     hammingThreeOne = HammingCode(3)
-    print(hammingThreeOne.H)
-    print(hammingThreeOne.G)
-    #print(hammingThreeOne.G)
-    #print(hammingThreeOne)
     #hammingSevenFour = HammingCode(3)
     #hammingFifteenEleven = HammingCode(4)
     print("End")
