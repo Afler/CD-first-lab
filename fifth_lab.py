f38029e4b97f09d049c2e9495f7aad78791ffd0c
Lev <afler777@gmail.com>
2021-11-20 20:04:20 +0400

    fifth lab started

--- fifth_lab.py
+++ fifth_lab.py
@@ -6,35 +6,74 @@ def printMatrix(arr, name):
     print(arr)
 
 
-def getGForRM(r, m):
-    if r == 0:
-        return np.mat(np.ones([1, 2 ** m]), dtype=int)
-    elif r == m:
-        rm = getGForRM(m - 1, m)
-        zeros = np.zeros([1, rm.shape[1]])
-        zeros[0, zeros.shape[1] - 1] = 1
-        return np.mat(np.vstack([rm, zeros]), dtype=int)
+def getG(r, m):
+    n = 2 ** m
+    G = np.mat(np.zeros(m), dtype=int)
+    for i in range(n):
+        G[[i]] = getVValues(I, m)
+    return G
+
+
+def getVValues(I, m):
+    v = np.mat(np.zeros([1, 2 ** m]), dtype=int)
+    x = fillX(m)
+    for i in range(v.shape[1]):
+        v[0, i] = f(x[0, i * m:(i + 1) * m], I)
+    return v
+
+
+def fillX(m):
+    answer = np.mat([], dtype=int)
+    binaryToAdd = np.mat(np.zeros([1, m]), dtype=int)
+    for i in range(2 ** m):
+        binaryIStr = (bin(i)[2:])[::-1]
+        for j in range(len(binaryIStr), m):
+            binaryIStr += "0"
+        for k in range(len(binaryIStr)):
+            binaryToAdd[0, k] = binaryIStr[k]
+        answer = np.hstack([answer, binaryToAdd])
+    return answer
+
+
+def f(x, I):
+    for i in range(I.shape[1]):
+        if x[0, I[0, i]] == 1:
+            return int(False)
+    return int(True)
+
+
+def С(n, k):
+    if 0 <= k <= n:
+        nn = 1
+        kk = 1
+        for t in range(1, min(k, n - k) + 1):
+            nn *= n
+            kk *= t
+            n -= 1
+        return nn // kk
     else:
-        GUpperLeft = getGForRM(r, m - 1)
-        GUpperRight = getGForRM(r, m - 1)
-        GLowerRight = getGForRM(r - 1, m - 1)
-        GLowerRight = np.mat(np.ravel(np.vstack([np.zeros([1, GUpperLeft.shape[0]]), GLowerRight])), dtype=int)
-        GRight = np.vstack([GUpperRight, GLowerRight])
-        GLeft = np.vstack([GUpperLeft, np.mat(np.zeros([GLowerRight.shape[0], GUpperLeft.shape[1]]))])
-        return np.mat(np.hstack([GRight, GLeft]), dtype=int)
+        return 0
 
 
-class RMCode:
+class CanonicalRMCode:
     r = 0
     m = 0
+    k = 0
+    d = 0
     G = np.mat([[]], dtype=int)
 
     def __init__(self, r, m):
         self.r = r
-        self.m = 2 ** m
-        self.G[[0]] = getGForRM(r, m)
+        self.m = m
+        self.n = 2 ** m
+        self.d = 2 ** (self.m - self.r)
+        for i in range(r + 1):
+            self.k += С(self.n, i)
+        self.G = getG(r, m)
 
 
 if __name__ == '__main__':
-    matrix = getGForRM(2, 2)
-    printMatrix(matrix, "matrix")
+    canonicalRM = CanonicalRMCode(3, 4)
+    v = getVValues(np.mat([2]), 4)
+    print(v)
+    print("End")
